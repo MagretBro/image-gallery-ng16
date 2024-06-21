d3fa9442ca660b903839a41c2ee9bd5322909565
MagretBro <magretbro@gmail.com>
2024-06-21 19:47:12 +0700

    writing tests

--- src/app/img-gallery/img-gallery.component.spec.ts
+++ src/app/img-gallery/img-gallery.component.spec.ts
@@ -18,4 +18,43 @@ describe('ImgGalleryComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('expect to be the first image', () => {
+    expect(component.currentImageIndex).toBe(0);
+  });
+
+  it('the index of image is correct', ()=> {
+    component.selectImage(2);
+    expect(component.currentImageIndex).toBe(2);
+  });
+
+  it('test prev image when prevImage is called', () =>{
+    component.currentImageIndex = 2;
+    component.prevImage();
+    expect(component.currentImageIndex).toBe(1);
+  })
+
+
+it('should wrap to the last image when prevImage is called from the first image', () => {
+  component.currentImageIndex = 0;
+  component.prevImage();
+  expect(component.currentImageIndex).toBe(component.images.length - 1);
+});
+
+it('should change to the next image when nextImage is called', () =>{
+  component.currentImageIndex = 0;
+  component.nextImage();
+  expect(component.currentImageIndex).toBe(1);
+});
+
+it('should wrap to the first image when nextImage is called from the last image', ()=> {
+  component.currentImageIndex = component.images.length - 1
+  component.nextImage();
+  expect(component.currentImageIndex).toBe(0);
+});
+
+//5
+
+
+
 });
